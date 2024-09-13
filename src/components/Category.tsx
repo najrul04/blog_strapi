63e5f52e8c6ee16c3875b76bc596a3c1cb816e42
MD Najrul Islam Sumon <nazrul2517@gmail.com>
2024-09-13 13:38:16 +0600

    first commit

---
+++ src/components/Category.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+import React from 'react'
+
+const Category = () => {
+  return (
+    <div onClick={() => {}} className='bg-[#af8533] p-4 rounded-lg shadow-md cursor-pointer'>Category</div>
+  )
+}
+
+export default Category
