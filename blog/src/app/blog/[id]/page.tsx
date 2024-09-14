9231f794cde04cc057db3d165b2b4106896790dc
MD Najrul Islam Sumon <nazrul2517@gmail.com>
2024-09-14 14:33:16 +0600

    fixing routing issue

---
+++ blog/src/app/blog/[id]/page.tsx
@@ -0,0 +1,30 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+const page = () => {
+  return (
+    <div className="max-w-3xl mx-auto p-4">
+      <Link href={"/"}>{"<Back"}</Link>
+      <div className="relative w-full h-96 overflow-hidden rounded-lg mt-5">
+        <Image src={""} alt={""} layout="fill" objectFit="cover" />
+      </div>
+      <div className="mt-4">
+        <h1 className="text-3xl font-semibold">
+          {"This is the Title of the Blog"}
+        </h1>
+        <p className="text-gray-600 mt-2">
+          {"This is the description of the blog"}
+        </p>
+        <div className="mt-4 flex items-center text-gray-400">
+          <span className="text-sm">
+            Published on {" "}
+            {"23-08-2023"}
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default page;
